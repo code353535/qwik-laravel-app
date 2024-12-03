59152f191494e553379354e967845b45d5d2d37b
code353535 <125445010+code353535@users.noreply.github.com>
2024-12-03 23:40:34 +0300

    a

---
+++ src/components/layouts/footer.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <footer class="bg-gray-800 p-4 text-white text-center">
+      <p>&copy; 2024 My Qwik App. All rights reserved.</p>
+    </footer>
+  );
+});
---
+++ src/components/layouts/header.tsx
@@ -0,0 +1,10 @@
+// Header.tsx
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <header class="bg-blue-600 p-4 text-white">
+      <h1>My Qwik App</h1>
+    </header>
+  );
+});
